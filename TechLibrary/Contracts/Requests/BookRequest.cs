741ba64d82f87602f0e7760956c891f7aa8586bf
RanjithPerumal123 <ranjith.perumal@gmail.com>
2020-03-27 20:29:01 -0700

    Add files via upload
    TeckLibrary

---
+++ TechLibrary/Contracts/Requests/BookRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechLibrary.Contracts.Requests
+{
+    public class BookRequest
+    {        
+            public int BookId { get; set; }
+            public string Title { get; set; }
+            public string ISBN { get; set; }
+            public string PublishedDate { get; set; }
+            public string ThumbnailUrl { get; set; }
+            public string Descr { get; set; }
+         
+    }
+}
---
+++ TechLibrary/Controllers/BooksController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using TechLibrary.Domain;
+using TechLibrary.Models;
+using TechLibrary.Services;
+using TechLibrary.Contracts.Requests;
+
+
+namespace TechLibrary.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BooksController : Controller
+    {
+        private readonly ILogger<BooksController> _logger;
+        private readonly IBookService _bookService;
+        private readonly IMapper _mapper;
+
+        public BooksController(ILogger<BooksController> logger, IBookService bookService, IMapper mapper)
+        {
+            _logger = logger;
+            _bookService = bookService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            _logger.LogInformation("Get all books");
+
+            var books = await _bookService.GetBooksAsync();
+
+            var bookResponse = _mapper.Map<List<BookResponse>>(books);
+
+            return Ok(bookResponse);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            _logger.LogInformation($"Get book by id {id}");
+
+            var book = await _bookService.GetBookByIdAsync(id);
+
+            var bookResponse = _mapper.Map<BookResponse>(book);
+
+            return Ok(bookResponse);
+        }
+
+        [HttpPost("{save}")]
+        public IActionResult SaveBook(BookRequest bookRequest )
+        {
+            _logger.LogInformation($"Save book Reqest");
+
+            var book = _bookService.SaveBook(bookRequest); 
+
+            return Ok(book);
+        }
+
+    }
+}
